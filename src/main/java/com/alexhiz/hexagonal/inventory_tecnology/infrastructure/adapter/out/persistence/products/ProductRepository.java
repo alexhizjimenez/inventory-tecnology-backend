f791f230720395b71760ebae08bb40d50904352d
alexis <alexhizdev@gmail.com>
2026-06-14 21:20:37 -0600

    feat(product): implement optimistic locking for stock management

--- src/main/java/com/alexhiz/hexagonal/inventory_tecnology/infrastructure/adapter/out/persistence/products/ProductRepository.java
+++ src/main/java/com/alexhiz/hexagonal/inventory_tecnology/infrastructure/adapter/out/persistence/products/ProductRepository.java
@@ -1,10 +1,24 @@
 package com.alexhiz.hexagonal.inventory_tecnology.infrastructure.adapter.out.persistence.products;
 
+import java.util.Optional;
 import java.util.UUID;
 
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProductRepository extends JpaRepository<ProductEntity, UUID> {
+
+   /* @Lock(LockModeType.PESSIMISTIC_WRITE)
+        @Query("""
+            SELECT p
+            FROM ProductEntity p
+            WHERE p.id = :id
+        """)
+        Optional<ProductEntity> findByIdWithLock(UUID id);
+    */
+
 }
 
     
