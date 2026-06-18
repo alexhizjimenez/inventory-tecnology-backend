bba70e04d55ef457bb8ec8480162e08f384505c8
alexis <alexhizdev@gmail.com>
2026-06-18 13:13:44 -0600

    refactor: decouple application services from Spring using explicit BeanConfig

--- src/main/java/com/alexhiz/hexagonal/inventory_tecnology/application/services/UserService.java
+++ src/main/java/com/alexhiz/hexagonal/inventory_tecnology/application/services/UserService.java
@@ -19,7 +19,7 @@ import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
-@Service
+
 @RequiredArgsConstructor
 public class UserService implements CreateUserUserCase, GetUserUseCase, ListUserUserCase {
 
